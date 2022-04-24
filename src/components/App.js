bd5e4287b385a217434fa8d86f29c91e29371f8a
codedog001 <mohitjain9875@gmail.com>
2022-04-24 17:10:29 +0530

    Update title and source

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
       </div>
       <footer>
         <p>Double click to edit a todo <br /> When done editing, press Enter to confirm changes <br /> Click on X to remove todo from the list </p>
-        <p><a href="https://github.com/codedog001/todo_react_graphql" target={"_blank"} >View Source</a></p>
+        <p><a href="https://github.com/codedog001/todo_graphql_react" target={"_blank"} >View Source</a></p>
       </footer>
     </div>
   );
